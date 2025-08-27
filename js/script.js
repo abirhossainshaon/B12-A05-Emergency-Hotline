db6ab89b0f2973de843d565dcbc6f6c110e99c2c
Abir Hossain Shaon <smabirhossainshaon@gmail.com>
2025-08-27 10:25:41 +0600

    added a counts and DOM Elements

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// counts
+let heartCount = 0;
+let coinCount = 100;
+let copyCount = 0;
+
+// Elements
+const heartCounter = document.getElementById("heartCountDisplay");
+const coinCounter = document.getElementById("coinCountDisplay");
+const copyCounter = document.getElementById("copyCountDisplay");
+const cardContainer = document.getElementById("cardContainer");
+const historyList = document.getElementById("historyList");
+const clearHistoryBtn = document.getElementById("clearHistory");
